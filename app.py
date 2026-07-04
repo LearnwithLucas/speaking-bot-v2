6759af98764b2b08350708773c015cc50d994584
LearnwithLucas <lucaspotjer@gmail.com>
2026-07-04 19:26:48 +0200

    Register admin health command

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ from commands.topics import setup as setup_topics
 from commands.dictionary import setup as setup_dictionary, VocabPublisher
 from commands.ask_jerry import setup as setup_ask_jerry, AskJerryView, AskJerryViewNL
 from commands.admin_refresh import setup as setup_admin_refresh
+from commands.admin_health import setup as setup_admin_health
 from commands.community_guide import setup as setup_community_guide
 from commands.jokes import setup as setup_jokes
 from jobs.word_of_the_day import WordOfTheDayJob
@@ -114,8 +115,9 @@ class SpeakingBot(commands.Bot):
             dutch_guild_id=self.dutch_guild_id,
         )
 
-        # Load admin refresh command
+        # Load admin commands
         await setup_admin_refresh(self)
+        await setup_admin_health(self)
 
         # Load community guide command
         await setup_community_guide(self)
